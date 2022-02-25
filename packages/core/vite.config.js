46b3c4fe9b63728fb83bd1aac5677019f0f42e45
Joe Plahitko <joseph.plahitko@egov.com>
2022-02-25 00:38:13 -0500

    Initial build setup for core; changed build script name in packages

---
+++ packages/core/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig(({ command, mode }) => {
+	return {
+		build: {
+			lib: {
+				entry: path.resolve(__dirname, 'src/index.ts'),
+				name: 'UIFramework',
+				formats: ['es', 'cjs', 'umd'],
+				fileName: 'ui-framework'
+			}
+		},
+	}
+});
