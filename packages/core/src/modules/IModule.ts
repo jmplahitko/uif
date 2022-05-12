8532139c5c142541dbba0c5a685459acd8c0ea76
Joe Plahitko <joseph.plahitko@egov.com>
2022-05-12 00:19:20 -0400

    Solidified configure and ready concepts on IModule.

--- packages/core/src/modules/IModule.ts
+++ packages/core/src/modules/IModule.ts
@@ -1,15 +1,13 @@
 import { ICommandHooks } from '../commands/ICommandHooks';
-import { ServiceFactory } from '@ui-framework/ioc';
+import { Injectable, InjectableFactory, ServiceKey } from '@ui-framework/ioc';
 import { IState } from '../IState';
-
-export type TInjectable<T> = Static<T> | ServiceFactory<T>;
 
 export interface IModule<TState extends IState> {
 	name: string;
-	commands: Map<Static<any>, TInjectable<ICommandHooks<any>>>;
+	commands: Map<Static<any>, Injectable<ICommandHooks<any>>>;
 	state: TState;
-	services: Map<string|symbol, TInjectable<any>>;
-	providers: Map<string|symbol, TInjectable<any>>;
-	configure();
-	ready();
+	services: Map<ServiceKey, Injectable<any>>;
+	providers: Map<ServiceKey, Injectable<any>>;
+	configure?: InjectableFactory<void>;
+	ready?: InjectableFactory<void>;
 }
