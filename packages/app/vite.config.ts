3d5d766d82bbf9437c3b2df2756f9838bc355612
Joe Plahitko <joseph.plahitko@egov.com>
2022-05-12 17:00:22 -0400

    Initial massage of test app's vite configuration that chunks out ui-framework for testing chunk sizes.

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -2,9 +2,26 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command, mode }) => ({
 	root: './',
+	define: {
+		__DEBUG__: mode === 'development'
+	},
 	plugins: [
 		vue(),
-	]
-})
+	],
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					// console.log(id)
+					const match = id.match(/next-framework\/packages\/(\w*)/);
+					if (match) {
+						// console.log(match)
+						return `ui-framework-${match[1]}`;
+					}
+				}
+			}
+		}
+	}
+}))
