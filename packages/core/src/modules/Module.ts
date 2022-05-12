3e29f9e7b7bcd24e1ab80ad93b420f1d9de48ba7
Joe Plahitko <joseph.plahitko@egov.com>
2022-05-12 16:58:53 -0400

    Removed Module.ts imports until it's actually built.

--- packages/core/src/modules/Module.ts
+++ packages/core/src/modules/Module.ts
@@ -1,10 +1,3 @@
-import { IRouteDescription } from '@ui-framework/navigation';
-import { IStore } from '@ui-framework/storage';
-import { IActionContext } from '../commands/ICommandContext';
-import { IActionPlan } from '../commands/ICommandHooks';
-import { IState } from '../IState';
-import { IModule, TInjectable } from './IModule';
-
 export class Module {
 	// TODO
 }
