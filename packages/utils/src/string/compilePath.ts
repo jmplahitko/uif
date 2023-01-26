78ed502d654dbd7a3a805ce7121912ee746d40ad
Joe Plahitko <joseph.plahitko@tylertech.com>
2023-01-26 10:16:14 -0500

    Reworked compilePath to work with path-to-regexp@6.2.1

--- packages/utils/src/string/compilePath.ts
+++ packages/utils/src/string/compilePath.ts
@@ -1,31 +1,23 @@
 import pathToRegexp from 'path-to-regexp';
-import { PathFunctionOptions } from 'path-to-regexp';
-import isObject from '../predicate/isObject';
+import isString from '../predicate/isString';
 
-export default function compilePath(path: string, data: object = {}, options: PathFunctionOptions = {}): string {
+function compilePath(path: string, data: object = {}): string {
 	data = Object.assign({}, data);
 	let tokens = pathToRegexp.parse(path);
-	let shouldCustomEncode = false;
-	let customEncode = (value: string) => value;
 
 	// any
 	tokens.forEach(token => {
-		if (isObject(token)) {
-			// @ts-ignore
-			if (!token.optional && !data.hasOwnProperty(token.name)) {
-				// @ts-ignore
+		if (!isString(token)) {
+			if (!data.hasOwnProperty(token.name)) {
 				data[token.name] = `:${token.name}`;
-				shouldCustomEncode = true;
 			}
 		}
 	});
 
-	if (!options.encode && shouldCustomEncode) {
-		options.encode = customEncode;
-	}
-
-	let toPath = pathToRegexp.compile(path);
-	let compiledPath = toPath(data, options);
+	let toPath = pathToRegexp.compile(path, { encode: encodeURIComponent });
+	let compiledPath = toPath(data);
 
 	return compiledPath;
 }
+
+export default compilePath;
