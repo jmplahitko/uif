aab35b97c620390beb942f863482a0bb30636dfe
Joe Plahitko <joseph.plahitko@tylertech.com>
2023-01-26 22:55:35 -0500

    Exposed constant, provider, and service register functions from index.

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,9 @@
+
 export { register as configure } from './startup/configurations'
+export { register as constant } from './startup/constants';
+export { register as provider } from './startup/providers';
 export { register as ready } from './startup/ready';
+export { register as service } from './startup/services';
 export { start } from './startup/start';
 export { useModule } from './modules/useModule';
 export { usePlugin } from './plugins/usePlugin';
