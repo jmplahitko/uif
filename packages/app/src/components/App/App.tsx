8135b6606b9dea272a897cba467609bc5c2c31dc
Joe Plahitko <joseph.plahitko@tylertech.com>
2023-01-26 23:17:48 -0500

    Fixed image in app

--- packages/app/src/components/App/App.tsx
+++ packages/app/src/components/App/App.tsx
@@ -1,13 +1,14 @@
 import './App.scss';
 import { defineComponent, h } from 'vue';
 import { RouterView } from 'vue-router';
+import img from '../../assets/logo.png';
 
 export default defineComponent({
   name: 'App',
 	render() {
 		return (
 			<div>
-				<img alt="Vue logo" src="../assets/logo.png" />
+				<img alt="Vue logo" src={img} />
 				<RouterView />
 			</div>
 		)
